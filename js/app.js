dcb9c14fae44bd32fd6be3fbd2dead2d5d662de4
Simon Kunz <pattexman@web.de>
2015-01-22 20:37:11 +0100

    noch besser

--- js/app.js
+++ js/app.js
@@ -8,10 +8,6 @@ $(window).scroll(function (){
     topPos = $('body').offset().top;        // = 0 weil body offset zum 0 punkt
     windowTop = $(window).scrollTop();      // window position
 
-    console.log('topPos' + topPos + offset);
-    console.log('windowTop' + windowTop);
-
-
     if(windowTop > topPos + offset){         // solange dein bildausschnitt größer des 0 punktes + offset ist
         $('#navigation').addClass('scroll');
     } else {
@@ -21,9 +17,21 @@ $(window).scroll(function (){
 });
 
 $('a[href^=#]').on('click', function(e){
-    var href = $(this).attr('href');
-    $('html, body').animate({
-        scrollTop:$(href).offset().top - navHeight     // zum Element springen und 10px hochfahren weil wegen fixed header
-    },'fast');
+    var href = $(this).attr('href'),
+        windowPosition = $(window).scrollTop();
+
+    if ($(href).offset().top > windowPosition) {
+        $('html, body').animate({
+            scrollTop:$(href).offset().top - navHeight * 2  // zum Element springen und 10px hochfahren weil wegen fixed header
+        },'fast');
+        console.log('1');
+    }else {
+        $('html, body').animate({
+            scrollTop:$(href).offset().top - navHeight  // zum Element springen und 10px hochfahren weil wegen fixed header
+        },'fast');
+        console.log('2');
+    }
+
+
     e.preventDefault();
 });
