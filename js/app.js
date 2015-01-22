b8dc3200ccd727a3961d146541aa6de8700f6093
Simon Kunz <simon.kunz@pixolith.de>
2015-01-22 14:48:24 +0100

    changes for app.js

--- js/app.js
+++ js/app.js
@@ -1,9 +1,19 @@
+var offset = 133,// header offset zur navigation in px
+    topPos,
+    windowTop,
+    startMenu = $('body').find('#startMenu');
+
 
 $(window).scroll(function (){
 
-    console.log(window.scrollY);
+    topPos = $('body').offset().top;        // = 0 weil body offset zum 0 punkt
+    windowTop = $(window).scrollTop();      // window position
 
-    if(window.scrollY > 133){
+    console.log('topPos' + topPos + offset);
+    console.log('windowTop' + windowTop);
+
+
+    if(windowTop > topPos + offset){         // solange dein bildausschnitt größer des 0 punktes + offset ist
         $('#navigation').addClass('scroll');
     } else {
         $('#navigation').removeClass('scroll');
@@ -11,12 +21,6 @@ $(window).scroll(function (){
 
 });
 
-$(function() {
-    $('#startMenu').on('click', function() {
-        $('#news').addClass('topSpaceScroll');
-    });
-});
-
-$(window).scrollTop(function() {
-    $('#news').removeClass('topSpaceScroll');
+$(window).click(startMenu , function() {       // scroll top mit offset on click
+    $(window).scrollTop(offset);
 });
